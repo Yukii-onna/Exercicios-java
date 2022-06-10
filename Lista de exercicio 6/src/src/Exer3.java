ab88495b8b8fedfac6e77146bbd6e04805be4bb1
Pedro Henrique <101288724+Yukii-onna@users.noreply.github.com>
2022-06-10 12:04:59 -0300

    Add files via upload

--- Lista de exercicio 6/src/src/Exer3.java
+++ Lista de exercicio 6/src/src/Exer3.java
@@ -1,56 +1,30 @@
-package src;
-
 import java.util.Scanner;
 
 public class Exer3 {
 
 	public static void main(String[] args) {
 		Scanner ent = new Scanner(System.in);
-		String dia;
+		String dia,mes;
+		String [] meses = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
 		StringBuilder ndia = new StringBuilder();
 		System.out.println("Digite o dia de nascimento: (dd/mm/aa)");
 		dia = ent.nextLine();
 		dia = dia.trim();
-		for (int i = 0; i < 2; i++) {
-			ndia.append(dia.charAt(i));
-		}
+		ndia.append(dia.substring(0, 2));
+		
+		ndia.append(" de ");
+		
+		mes = dia.substring(3,5);
+		
+		ndia.append(meses[Integer.parseInt(mes)-1]);
 
 		ndia.append(" de ");
 
-		if (dia.charAt(3) == '0' && dia.charAt(4) == '1') {
-			ndia.append("Janeiro");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '2') {
-			ndia.append("Fevereiro");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '3') {
-			ndia.append("Março");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '4') {
-			ndia.append("Abril");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '5') {
-			ndia.append("Maio");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '6') {
-			ndia.append("Junho");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '7') {
-			ndia.append("Julho");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '8') {
-			ndia.append("Agosto");
-		} else if (dia.charAt(3) == '0' && dia.charAt(4) == '9') {
-			ndia.append("Setembro");
-		} else if (dia.charAt(3) == '1' && dia.charAt(4) == '0') {
-			ndia.append("Outubro");
-		} else if (dia.charAt(3) == '1' && dia.charAt(4) == '1') {
-			ndia.append("Novembro");
-		} else if (dia.charAt(3) == '1' && dia.charAt(4) == '2') {
-			ndia.append("Dezembro");
-		}
-
-		ndia.append(" de ");
-
-		for (int i = 6; i < dia.length(); i++) {
-			ndia.append(dia.charAt(i));
-		}
+		ndia.append(dia.substring(6,10));
 
 		System.out.printf("%s", ndia);
 		ent.close();
+
 	}
 
 }
